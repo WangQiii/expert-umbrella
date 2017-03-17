bda485aee35f156179496602dd4954ef11e7ce13
WangQiii <wq_007@qq.com>
2017-03-17 10:33:31 +0800

    添加项目文件。

---
+++ WebApiDemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft;
+using Newtonsoft.Json.Serialization;
+
+namespace WebApiDemo
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 配置和服务
+            var globalCors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(globalCors);
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver=
+                new CamelCasePropertyNamesContractResolver();
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApiDemo/Controllers/BookController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class BookController : ApiController
+    {
+        List<Book> bookList = new List<Book>()
+        {
+            new Book(){Id="1",Name="哈姆雷特"},
+            new Book(){ Id="2",Name="西游记"}
+        };
+
+        [Route("api/book")]
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return bookList;
+        }
+
+        [Route("api/book/{id}")]
+        [HttpGet]
+        public Book Get(string id)
+        {
+            return bookList.First(f => f.Id == id);
+        }
+
+        [Route("api/book")]
+        [HttpPost]
+        public void Post([FromBody]Book book)
+        {
+            bookList.Add(book);
+        }
+
+        [Route("api/book/{id}")]
+        [HttpPut]
+        public void Put(string id, [FromBody]string name)
+        {
+            var book = bookList.First(f => f.Id == id);
+            book.Name = name;
+        }
+
+        [Route("api/book/{id}")]
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            bookList.Remove(bookList.First(f => f.Id == id));
+        }
+    }
+}
